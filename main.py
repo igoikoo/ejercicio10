4f90f9cfb8dd833f7007f418725dd0139bc36d46
iñigo <igoikoetxeal@alumnos.nebrija.es>
2022-01-18 18:33:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import statistics
+import StatsData as sd
+lista_numeros = []
+while True:
+    numeros = input("Dime numeros: ")
+    if numeros == "fin":
+        break
+    numeros = float(numeros)
+    lista_numeros.append(numeros)
+res = sd.StatsData(lista_numeros)
+print("Lista números: ", res.l_data)
+print("Media: ", res.mean)
+print("Mediana: ", res.median)
+print("Varianza: ", res.variance)
